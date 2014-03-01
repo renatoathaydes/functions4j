970302e2665704d614e8075ef8d3f93fe12a73ea
renato <renatoathaydes@hotmail.com>
2014-03-01 10:29:54 +0100

    Documented Ordering and added example for OrderingMonoid

---
+++ examples/monoid/OrderingExample.java
@@ -0,0 +1,59 @@
+package monoid;
+
+import com.athaydes.functions4j.compare.OrderingMonoid;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.athaydes.functions4j.compare.Ordering.asOrdering;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Renato
+ */
+public class OrderingExample {
+
+    public static class Person {
+        final String fname;
+        final String lname;
+        final int age;
+
+        public Person( String fname, String lname, int age ) {
+            this.fname = fname;
+            this.lname = lname;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + fname + "," + lname + "," + age + ")";
+        }
+
+    }
+
+    private OrderingMonoid orderingM = new OrderingMonoid();
+
+    @Test
+    public void usingTheOrderingMonoidToSortPeopleByEachField() {
+        List<Person> people = Arrays.asList(
+                new Person( "John", "Meyer", 27 ),
+                new Person( "Mary", "Jane", 25 ),
+                new Person( "Hans", "Hansson", 39 ),
+                new Person( "Julia", "Roberts", 41 ),
+                new Person( "Julia", "Roberts", 22 ),
+                new Person( "Hans", "Schneider", 19 ) );
+
+        people.sort( ( p1, p2 ) -> orderingM.applyOn( Stream.of(
+                asOrdering( p1.fname, p2.fname ),
+                asOrdering( p1.lname, p2.lname ),
+                asOrdering( p1.age, p2.age )
+        ) ).intValue );
+
+        assertEquals( people.toString(),
+                "[(Hans,Hansson,39), (Hans,Schneider,19), (John,Meyer,27), (Julia,Roberts,22)," +
+                        " (Julia,Roberts,41), (Mary,Jane,25)]" );
+    }
+
+}
--- src/main/java/com/athaydes/functions4j/compare/Ordering.java
+++ src/main/java/com/athaydes/functions4j/compare/Ordering.java
@@ -1,6 +1,14 @@
 package com.athaydes.functions4j.compare;
 
 /**
+ * Enumerator that represents the result of comparing two values.
+ * <p>
+ * LT = Less Than
+ * <p>
+ * GT = Greater Than
+ * <p>
+ * EQ = Equal
+ *
  * @author Renato
  */
 public enum Ordering {
@@ -13,12 +21,35 @@ public enum Ordering {
         this.intValue = intValue;
     }
 
+    /**
+     * Converts the given int value to an Ordering using the convention:
+     * <ul>
+     * <li>0   - EQ</li>
+     * <li>< 0 - LT</li>
+     * <li>> 0 - GT</li>
+     * </ul>
+     *
+     * @param intValue
+     * @return Ordering from intValue
+     */
     public static Ordering asOrdering( int intValue ) {
         return intValue == 0 ?
                 EQ :
                 intValue > 0 ?
                         GT :
                         LT;
+    }
+
+    /**
+     * Compares the two given values, returning an Ordering representing the result of the comparison.
+     *
+     * @param c1  first value
+     * @param c2  second value
+     * @param <T> type of values being compared
+     * @return Ordering resulting from comparing the given values
+     */
+    public static <T> Ordering asOrdering( Comparable<T> c1, T c2 ) {
+        return asOrdering( c1.compareTo( c2 ) );
     }
 
 }
